97c592620e07eaf4f2a5cf2f71da5114f730e420
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-06 09:02:20 +0530

    dashboard

--- expensestracker/app/page.js
+++ expensestracker/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { buttonVariants } from "@/components/ui/button"
+import dashboard from './dashboard/page';
 
 
 export default function Home() {
   return (
     <>
-    <button variants="outline">Subscribe</button>
+    <dashboard/>
     </>
   );
 }
