4994d2215e0b567589914380ac9a5f615219047c
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-06 19:58:17 +0530

    create clerk

--- expensestracker/app/layout.js
+++ expensestracker/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono,Outfit} from "next/font/google";
 import "./globals.css";
-
+import {
+  ClerkProvider,
+ 
+} from '@clerk/nextjs'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,6 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -30,5 +34,6 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
