ee467110783f4aae90b85968c1b668841ed3d556
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-07 14:52:06 +0530

    create layout for dashbord

---
+++ expense-tracker/app/(routes)/dashboard/_components/SideNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SideNav() {
+  return (
+    <div>SidNav</div>
+  )
+}
+
+export default SideNav
---
+++ expense-tracker/app/(routes)/dashboard/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideNav from 
+
+function DashboardLayout({children}) {
+  return (
+    <div>
+         div
+         
+    </div>
+  )
+}
+
+export default DashboardLayout
