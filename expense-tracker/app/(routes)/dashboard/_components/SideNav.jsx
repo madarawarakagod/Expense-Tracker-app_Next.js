b2eee807e62b3acb5379cb0522b943be06653c35
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-10 11:34:44 +0530

    change dashboard

--- expense-tracker/app/(routes)/dashboard/_components/SideNav.jsx
+++ expense-tracker/app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import Image from 'next/image'
 import { LayoutGrid, PiggyBank,ReceiptText,ShieldCheck } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
+
 
 function SideNav() {
 
+  
+  
   const menuList=[
     {
       id:1,
       name:'Dashboard',
-      icon:LayoutGrid
+      icon:LayoutGrid,
+      path:'/dashboard'
     },
     {
       id:2,
       name:'Budgets',
-      icon:PiggyBank
+      icon:PiggyBank,
+      path:'/dashboard/budgets'
     },
     {
       id:3,
       name:'Expenses',
-      icon:ReceiptText
+      icon:ReceiptText,
+      path:'/dashboard/expenses'
+
     },
     {
       id:4,
       name:'Upgrade',
-      icon:ShieldCheck
+      icon:ShieldCheck,
+      path:'/dashboard/upgrade'
     }
   ]
 
+  const path=usePathname();
+
+  useEffect(()=>{
+    console.log(path)
+  },[path])
 
 
 
@@ -43,11 +57,12 @@ function SideNav() {
 
     <div className='mt-5'> 
     {menuList.map((menu, index)=>( 
-    <h2 className='flex gap-2 items-center 
+    <h2 className={`flex gap-2 items-center 
     text-gray-500 font-medium 
     p-5 cursor-pointer rounded-md 
     hover: text-primary hover:bg-purple-300
-    '>
+    ${path==menu.path&& 'text-primary bg-blue-100'}
+    `}>
     <menu.icon/> 
     {menu.name} 
     </h2> 
