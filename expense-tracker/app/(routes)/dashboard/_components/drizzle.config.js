5bdc6aa9458e8b7c10802baac3c973d4086454dc
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-10 23:39:39 +0530

    drizzle

--- expense-tracker/app/(routes)/dashboard/_components/drizzle.config.js
+++ expense-tracker/app/(routes)/dashboard/_components/drizzle.config.js
@@ -4,6 +4,6 @@ export default {
   schema: './utils/schema.jsx',
   dialect: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: postgresql://neondb_owner:npg_m9AfBtbQDJ2z@ep-morning-sun-a4niyk9t-pooler.us-east-1.aws.neon.tech/expense?sslmode=require
   },
 };
--- expense-tracker/utils/dbConfig.jsx
+++ expense-tracker/utils/dbConfig.jsx
@@ -2,5 +2,5 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(postgresql://neondb_owner:npg_m9AfBtbQDJ2z@ep-morning-sun-a4niyk9t-pooler.us-east-1.aws.neon.tech/expense?sslmode=require);
 const db = drizzle(sql,{schema});
