fc75e1fddada3a1c4e3ba472d11e943988ac638d
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-11 01:53:19 +0530

    update drizzle

--- expense-tracker/drizzle.config.js
+++ expense-tracker/drizzle.config.js
@@ -1,8 +1,22 @@
 
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_m9AfBtbQDJ2z@ep-morning-sun-a4niyk9t-pooler.us-east-1.aws.neon.tech/expense?sslmode=require"
+  },
+});
+
+
+
+/*
 export default {
   schema: './utils/schema.jsx',
-  dialect: 'postgresql',
+  dialect: 'pg',
   dbCredentials: {
     connectionString:'postgresql://neondb_owner:npg_m9AfBtbQDJ2z@ep-morning-sun-a4niyk9t-pooler.us-east-1.aws.neon.tech/expense?sslmode=require',
   },
 };
+*/
