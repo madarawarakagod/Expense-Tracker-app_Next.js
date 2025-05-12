b8aaa0af8c64ba75f02bd5986423b1b50865340d
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-12 13:48:12 +0530

    create expenses

--- expense-tracker/utils/schema.jsx
+++ expense-tracker/utils/schema.jsx
@@ -1,4 +1,4 @@
-import { pgTable,serial,varchar} from "drizzle-orm/pg-core";
+import {integer,numeric,pgTable,serial,varchar} from "drizzle-orm/pg-core";
 
 
 export const Budgets=pgTable('budgets',{ 
@@ -7,4 +7,12 @@ export const Budgets=pgTable('budgets',{
       amount: varchar('amount').notNull(), 
       icon: varchar('icon'), 
       createdBy: varchar('createdBy').notNull()
+})
+
+export const Expenses=pgTable('expenses',{ 
+      id:serial('id').primaryKey(), 
+      name:varchar('name').notNull(), 
+      amount:numeric('amount').notNull().default(0), 
+      budgetId:integer('budgetId').references(()=>Budgets.id), 
+      createdAt:varchar('createdAt').notNull() 
 })
