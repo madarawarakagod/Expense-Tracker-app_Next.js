76bd42307ea1a5cd96b2b944e4e772e7b6a99010
madarawarakagoda <madarawarakagoda8@gmail.com>
2025-05-16 11:54:17 +0530

    expense list

--- expense-tracker/app/(routes)/dashboard/page.jsx
+++ expense-tracker/app/(routes)/dashboard/page.jsx
@@ -13,11 +13,15 @@ function Dashboard()
  
     const {user}=useUser();
     const [budgetList,setBudgetList]=useState([]);
+    const [expensesList,setExpensesList]=useState([]);
     
   
     useEffect(()=>{
         user&&getBudgetList();  
     },[user])
+
+    
+    
     
           const getBudgetList=async()=> { 
     
@@ -34,6 +38,26 @@ function Dashboard()
     
     
             setBudgetList(result);
+            getAllExpenses();
+
+        }
+
+            /**
+     * used to get budget Lsit
+     */
+            const getAllExpenses=async()=>{ 
+            const result=await db.select({ 
+            id: Expenses.id, 
+            name: Expenses.name, 
+            amount: Expenses.amount, 
+           createdAt: Expenses.createdAt
+            }).from(Budgets)
+            .rightJoin(Expenses, eq(Budgets.id, Expenses.budgetId)) 
+            .where(eq(Budgets.createdBy,user?.primaryEmailAddress.emailAddress))
+            .orderBy (desc(Expenses.id));
+             setExpensesList(result);
+       
+            
     
   }
 
@@ -48,6 +72,11 @@ function Dashboard()
         <BarchartDashboard
         budgetList={budgetList}
         />
+        <ExpensesListTable
+         expensesList={expensesList}
+         refreshData={()=>getBudgetList()}
+        />
+
         </div>
         <div className='grid gap-3'>
           <h2 className='font-bold text-lg'>Latest Budgets</h2>
